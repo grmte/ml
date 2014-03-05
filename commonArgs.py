531d6f007aadad8be1e8ab57fa063252e5f923f7
VK <kedia.vikas@gmail.com>
2014-03-04 20:50:56 -0800

    better code

--- commonArgs.py
+++
@@ -1,5 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='Ths program will write a csv file that will have the targets. An e.g. command line is python tGen.py -d ../data/20140207/')
-parser.add_argument('-d', required=True,help='Location of the data file')
-parser.add_argument('-m', required=True,help='Location of code module')
-args = parser.parse_args()
