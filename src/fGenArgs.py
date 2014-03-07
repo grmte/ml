00c21b18938c5a8b59d0fef5c157f68f08245ca9
VK <kedia.vikas@gmail.com>
2014-03-07 02:56:08 -0800

    bc

--- src/fGenArgs.py
+++ src/fGenArgs.py
@@ -1,5 +1,5 @@
 import argparse
-parser = argparse.ArgumentParser(description='Ths program will write a csv file that will have the features. An e.g. command line is python fGen.py -d ../data/20140207/ -m fLTPOfCurrentRow')
+parser = argparse.ArgumentParser(description='Ths program will write a csv file that will have the features. An e.g. command line is python fGen.py -d ob/data/20140207/ -m ob/fLTPOfCurrentRow')
 parser.add_argument('-d', required=True,help='Location of the data file')
 parser.add_argument('-m', required=True,help='Location of code module')
 args = parser.parse_args()
--- src/tGenArgs.py
+++ src/tGenArgs.py
@@ -1,5 +1,5 @@
 import argparse
-parser = argparse.ArgumentParser(description='Ths program will write a csv file that will have the targets. An e.g. command line is: ./tGen.py -d ../data/20140207/ -m tBidGreaterThanAskInNext100')
+parser = argparse.ArgumentParser(description='Ths program will write a csv file that will have the targets. An e.g. command line is: ./tGen.py -d ob/data/20140207/ -m ob/tBidGreaterThanAskInNext100')
 parser.add_argument('-d', required=True,help='Location of the data file')
 parser.add_argument('-m', required=True,help='Location of code module')
 args = parser.parse_args()
