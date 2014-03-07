ef0933965ecaf81747f35896e487a92919b27280
VK <kedia.vikas@gmail.com>
2014-03-06 23:13:51 -0800

    bc

--- src/tGen.py
+++ src/tGen.py
@@ -1,21 +1,27 @@
 #!/usr//bin/python
 import os
 import sys
-sys.path.append(os.getcwd())
-import colNumberOfData
+import importlib
+
 import dataFile
 import tGenArgs
 import target
-import importlib
 
-user_module = importlib.import_module(tGenArgs.args.m)
+sys.path.append(os.path.dirname(tGenArgs.args.m))
+try:
+   import colNumberOfData
+except:
+   print "There is some problem with the path. I cannot import colNumberOfData"
+   os._exit(-1)
+
+user_module = importlib.import_module(os.path.basename(tGenArgs.args.m))
 
 
 def main():
    dataFile.getDataIntoMatrix(tGenArgs.args.d)
    target.initVector()
    user_module.extractTargetFromDataMatrix()
-   target.writeToFile(tGenArgs.args.m)
+   target.writeToFile(os.path.basename(tGenArgs.args.m))
 
 if __name__ == "__main__":
     main()
