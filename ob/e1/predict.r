0607792945a0c76235f9714f678c9839beb8aa62
VK <kedia.vikas@gmail.com>
2014-03-06 23:22:51 -0800

    bc

--- ob/e1/predict.r
+++ ob/e1/predict.r
@@ -9,7 +9,7 @@ feature1=read.csv("fBidP0OfCurrentRow.feature", header=FALSE)
 feature2=read.csv("fLTPOfCurrentRow.feature", header=FALSE) 
 
 print ("Section3: Read in the prediction model") 
-load("e1/design.model")
+load("ob/e1/design.model")
 
 print ("Section4: Creating the data frame") 
 df = data.frame(fBidP0OfCurrentRow=feature1$V2,fLTPOfCurrentRow=feature2$V2)
@@ -18,5 +18,5 @@ print ("Section5: Running logistic regression prediction")
 df$Prob <- predict (logistic.fit, newdata = df, type = "response")
 
 
-print ("Section6: Saving the predictions in directory ~/Downloads/src/data/20140207/ in file e1/design.predictions") 
-write.table(df, file = "e1/design.predictions")
+print ("Section6: Saving the predictions in directory ~/Downloads/src/data/20140207/ in file ob/e1/design.predictions") 
+write.table(df, file = "ob/e1/design.predictions")
