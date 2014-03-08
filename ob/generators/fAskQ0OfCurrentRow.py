711b99944a6f9edd5415e6e20643e0375d19c24e
VK <kedia.vikas@gmail.com>
2014-03-08 13:15:43 -0800

    bc

--- ob/generators/fAskQ0OfCurrentRow.py
+++ ob/generators/fAskQ0OfCurrentRow.py
@@ -5,8 +5,8 @@ import feature
 def extractFeatureFromDataMatrix():
    currentRowCount = 0
    for dataRow in dataFile.matrix:
-      askP0OfCurrentRow = float(dataFile.matrix[currentRowCount][colNumberOfData.AskP0])
+      askQ0OfCurrentRow = float(dataFile.matrix[currentRowCount][colNumberOfData.AskQ0])
       feature.vector[currentRowCount][0] = dataFile.matrix[currentRowCount][colNumberOfData.TimeStamp]
-      feature.vector[currentRowCount][1] = askP0OfCurrentRow
+      feature.vector[currentRowCount][1] = askQ0OfCurrentRow
       currentRowCount = currentRowCount + 1
       print "Processed row number " + str(currentRowCount)
--- ob/generators/fBidQ0OfCurrentRow.py
+++ ob/generators/fBidQ0OfCurrentRow.py
@@ -5,8 +5,8 @@ import feature
 def extractFeatureFromDataMatrix():
    currentRowCount = 0
    for dataRow in dataFile.matrix:
-      askP0OfCurrentRow = float(dataFile.matrix[currentRowCount][colNumberOfData.AskP0])
+      bidQ0OfCurrentRow = float(dataFile.matrix[currentRowCount][colNumberOfData.BidQ0])
       feature.vector[currentRowCount][0] = dataFile.matrix[currentRowCount][colNumberOfData.TimeStamp]
-      feature.vector[currentRowCount][1] = askP0OfCurrentRow
+      feature.vector[currentRowCount][1] = bidQ0OfCurrentRow
       currentRowCount = currentRowCount + 1
       print "Processed row number " + str(currentRowCount)
