bd93f733f5b89e8cb0a605a405ce1637ede7af1e
VK <kedia.vikas@gmail.com>
2014-03-09 16:54:08 -0700

    bc

--- src/pGen.py
+++ src/pGen.py
@@ -21,11 +21,11 @@ dirName=os.path.dirname(args.e)
 f = open(dirName+'/predict.r','w')
 
 f.write('#!/usr/bin/Rscript \n')
+
 f.write('print ("Section1: Setting the environment") \n')
 f.write('rm(list=ls()) \n')
 
 f.write('args <- commandArgs(trailingOnly = TRUE) \n')
-
 f.write('if(length(args) < 2) \n')
 f.write('{ \n')
 f.write('  stop("Not enough arguments. Please supply 2 arguments.") \n')
@@ -37,26 +37,46 @@ f.write('}else{ \n')
 f.write('   stop ("cannot proceed. Specify the parameters properly. The correct way to use this is predict.r -d data/20140207") \n')    
 f.write('} \n')
 
-f.write('\nprint ("Section2: Read in the feature files") \n')
+f.write('print ("Section2: checking if the predictions file already exists") \n')
+f.write('fileName = paste(args[2],"' + os.path.basename(os.path.dirname(args.e)) +'.predictions",sep="") \n')
+f.write('if(file.exists(fileName)){ \n')
+f.write("    stop ('The predictions already exist. Delete it and then run the program again') \n")
+f.write("} \n")
+
+f.write('\nprint ("Section3: Read in the feature files") \n')
 features = config["features"]
 for feature in features:
     f.write('print ("Reading in '+ features[feature] +'.feature' + '") \n')
     f.write(feature+'=read.csv(paste(args[2],"'+features[feature]+'.feature",sep=""), header=FALSE) \n')
 
-f.write('\nprint ("Section3: Making sure all feature vectors are of same length and have same time stamp over each row") \n')
+f.write('\nprint ("Section4: Making sure all feature vectors are of same length") \n')
 features = config["features"]
 currentFeatureNumber = 0
 while currentFeatureNumber  <  (len(features) - 1) :
-  f.write('if (length(' + features.keys()[currentFeatureNumber] + ') != length(' + features.keys()[currentFeatureNumber+1] + ')) { \n')
-  f.write('print ("The feature lengths do not match. This is an error I cannot recover from") \n')
+  f.write('if (length(' + features.keys()[currentFeatureNumber] + '$V1) != length(' + features.keys()[currentFeatureNumber+1] + '$V1)) { \n')
+  f.write('print ("The feature lengths do not match for ' + features.keys()[currentFeatureNumber] + features.values()[currentFeatureNumber] +' and '+features.keys()[currentFeatureNumber+1]+ features.values()[currentFeatureNumber+1]+'") \n')
   f.write('quit() \n')
-  f.write('} \n')
+  f.write('}else{ \n')
+  f.write('print ("Length of ' + features.keys()[currentFeatureNumber] + ' is same as length of '+features.keys()[currentFeatureNumber+1] +'")\n')
+  f.write('}\n')
   currentFeatureNumber = currentFeatureNumber + 1
-f.write('\nprint ("Section4: Read in the prediction model") \n')
+
+f.write('\nprint ("Section5: Making sure all feature vectors have same time stamp over each row") \n')
+features = config["features"]
+currentFeatureNumber = 0
+while currentFeatureNumber  <  (len(features) - 1) :
+  f.write('if (all(' + features.keys()[currentFeatureNumber] + '$V1 == ' + features.keys()[currentFeatureNumber+1] + '$V1) != TRUE) { \n')
+  f.write('print ("The feature timestamps do not match for ' + features.keys()[currentFeatureNumber] + features.values()[currentFeatureNumber] +' and '+features.keys()[currentFeatureNumber+1]+ features.values()[currentFeatureNumber+1]+'") \n')
+  f.write('quit() \n')
+  f.write('}else{ \n')
+  f.write('print ("Timestamps of ' + features.keys()[currentFeatureNumber] + ' is same as timestamp of '+features.keys()[currentFeatureNumber+1] +'")\n')
+  f.write('}\n')
+  currentFeatureNumber = currentFeatureNumber + 1
+
+f.write('\nprint ("Section6: Read in the prediction model") \n')
 f.write('load("'+args.e+'/design.model")')
 
-
-f.write('\n\nprint ("Section5: Creating the data frame") \n')
+f.write('\n\nprint ("Section7: Creating the data frame") \n')
 f.write('df = data.frame(')
 currentFeatureNumber=0
 for feature in features:
@@ -66,21 +86,18 @@ for feature in features:
             f.write(',')
 f.write(")\n\n")
 
-f.write('print ("Section6: Running logistic regression prediction") \n')
+f.write('print ("Section8: Running logistic regression prediction") \n')
 f.write('df$Prob <- predict (logistic.fit, newdata = df, type = "response")')
 f.write("\n\n")
 
-f.write('\nprint ("Section7: Creating the data frame to write in the file") \n')
+f.write('\nprint ("Section9: Creating the data frame to write in the file") \n')
 f.write('dfForFile <- data.frame(df$Prob)')
 
-f.write('\nprint ("Section8: Putting the timestamps in the data frame as a sanity check mechanism") \n')
+f.write('\nprint ("Section10: Putting the timestamps in the data frame as a sanity check mechanism") \n')
 f.write('dfForFile <- cbind(dfForFile,'+features.keys()[0]+'$V1) \n')
 
-f.write('\nprint ("Section9: Saving the predictions in file '+ os.path.basename(os.path.dirname(args.e)) +'.predictions") \n')
+f.write('\nprint ("Section11: Saving the predictions in file '+ os.path.basename(os.path.dirname(args.e)) +'.predictions") \n')
 f.write('fileName = paste(args[2],"' + os.path.basename(os.path.dirname(args.e)) +'.predictions",sep="") \n')
-
-
-
 f.write('print (fileName) \n')
 f.write('write.table(dfForFile, file = fileName)')
 
