4c21b88dac6618e1397014233db772c3fc167f3c
VK <kedia.vikas@gmail.com>
2014-03-12 12:58:41 -0700

    bc

--- src/rGenAll.py
+++ src/rGenAll.py
@@ -4,14 +4,15 @@
 import argparse
 parser = argparse.ArgumentParser(description='This program will run mGen.py and pGen.py. An e.g. command line is rGenAll.py -e e1/')
 parser.add_argument('-e', required=True,help='Directory of the experiment')
+parser.add_argument('-a', required=True,help='Algorithm name')
 args = parser.parse_args()
 
 import subprocess
 
 print "Running mGen.py"
-subprocess.call(["mGen.py","-e",args.e])
+subprocess.call(["mGen.py","-e",args.e,"-a",args.a])
 
 print "Generating pGen.py"
-subprocess.call(["pGen.py","-e",args.e])
+subprocess.call(["pGen.py","-e",args.e,"-a",args.a])
 
     
