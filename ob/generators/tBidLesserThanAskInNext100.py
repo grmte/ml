85b2d68755063fd5eada74ed0f464f376885af36
VK <kedia.vikas@gmail.com>
2014-03-14 18:15:14 -0700

    bc

---
+++ ob/generators/tBidLesserThanAskInNext100.py
@@ -0,0 +1,26 @@
+import os
+import colNumberOfData
+import dataFile
+import tGenArgs
+import target
+import common
+
+def extractTargetFromDataMatrix():
+   """
+   In the next 100 order book entries is there a case where the bidP0 is > current askP0
+   """
+   currentRowCount = 0
+
+   while currentRowCount < len(dataFile.matrix)-101:
+      futureRowCount = 1
+
+      target.vector[currentRowCount][0] = common.getTimeStamp(dataFile.matrix[currentRowCount])
+      while(futureRowCount < 100):
+         if( float(dataFile.matrix[currentRowCount+futureRowCount][colNumberOfData.BidP0]) < float(dataFile.matrix[currentRowCount][colNumberOfData.AskP0]) ):
+            target.vector[currentRowCount][1] = 1
+         futureRowCount = futureRowCount + 1
+
+      currentRowCount = currentRowCount + 1
+      if(currentRowCount % 1000 == 0):
+         print "Processed row number " + str(currentRowCount) 
+
