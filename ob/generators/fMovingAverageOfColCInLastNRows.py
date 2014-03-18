698776e7cae90b0ee1407c8dd9f713547148af1c
VK <kedia.vikas@gmail.com>
2014-03-17 21:11:22 -0700

    bc

--- ob/generators/fMovingAverageOfColCInLastNRows.py
+++ ob/generators/fMovingAverageOfColCInLastNRows.py
@@ -3,8 +3,12 @@ import colNumberOfData
 import feature
 import common
 import fGenArgs
+from collections import deque
 
 def extractFeatureFromDataMatrix():
+   queueOfCellValueInLastNRows = deque()
+   totalOfLastNRows = 0.0
+   
    if fGenArgs.args.n == None:
       N = 5
    else:
@@ -18,24 +22,25 @@ def extractFeatureFromDataMatrix():
 
    currentRowCount = 0
 
-   codeString = 'float(dataFile.matrix[currentRowCount - i][colNumberOfData.'+ fGenArgs.args.c + ']) + ColSum'
    for dataRow in dataFile.matrix:
+
+      codeString = 'float(dataFile.matrix[currentRowCount][colNumberOfData.'+ fGenArgs.args.c + '])'
+      cellValue = eval(codeString)
+      queueOfCellValueInLastNRows.append(cellValue)
+      totalOfLastNRows += cellValue
 
       if (currentRowCount < N):
          feature.vector[currentRowCount][0] = common.getTimeStamp(dataFile.matrix[currentRowCount])
-         feature.vector[currentRowCount][1] = 0 # TODO: Figure out how to put NA here.
+         feature.vector[currentRowCount][1] = totalOfLastNRows/(currentRowCount+1) # in 1st iteration currentRowCount = 0
          currentRowCount = currentRowCount + 1
          continue     # Since we are going back 1 row from current we cannot get data from current row
       
-      ColSum = 0
-      i = 0
-      while i < N:
-         ColSum = eval(codeString)
-         i = i+1
 
+      totalOfLastNRows -= queueOfCellValueInLastNRows.popleft()
+     
       # In the next 2 rows we do not do -1 since this feature if for the current row.
       feature.vector[currentRowCount][0] = common.getTimeStamp(dataFile.matrix[currentRowCount])
-      feature.vector[currentRowCount][1] = float(ColSum)/ N
+      feature.vector[currentRowCount][1] = totalOfLastNRows / N
 
       currentRowCount = currentRowCount + 1
 
