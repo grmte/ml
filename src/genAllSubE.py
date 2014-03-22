57fdbacc28c770755ae01fbae64430804c7b2820
VK <kedia.vikas@gmail.com>
2014-03-22 15:13:00 -0700

    bc

--- src/genAllSubE.py
+++ src/genAllSubE.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-import itertools
-import argparse
+import itertools, os,argparse
 from configobj import ConfigObj
 
 parser = argparse.ArgumentParser(description='This program will run generate all the subexperiments. An e.g. command line is genAllSubE.py -e e1/')
@@ -13,13 +12,33 @@ config = ConfigObj(args.e+"/design.ini")
 features = config["features"]
 
 i = 1
+numberOfFeatureSet = 1
 numberOfFeatureSets = 0
 
 while i <= len(features):
     i += 1
-    featureSet = list(itertools.combinations(features, i))
-    print featureSet
-    numberOfFeatureSets += len(featureSet)
+    # lets make a directory if it does not already exist
+    try:
+        os.stat(args.e+"/s/"+str(i)+"c")
+    except:
+        os.mkdir(args.e+"/s/"+str(i)+"c")       
+
+    featureSets = list(itertools.combinations(features, i))
+    for featureSet in featureSets:
+        newConfig = ConfigObj()
+        try:
+            os.stat(args.e+"/s/"+str(i)+"c/"+''.join(featureSet))
+        except:
+            os.mkdir(args.e+"/s/"+str(i)+"c/"+''.join(featureSet))       
+        newConfig.filename = args.e+"/s/"+str(i)+"c/"+''.join(featureSet) + "/design.ini"
+        newConfig['target'] = config['target']
+        newConfig['features'] = {}
+        for feature in featureSet:
+            newConfig['features'][feature] = features[feature]
+        newConfig.write()
+        numberOfFeatureSet += 1
+
+    numberOfFeatureSets += len(featureSets)
 
 print "The total number of features are " + str(len(features))
 print "The total number of feature sets are " + str(numberOfFeatureSets)
