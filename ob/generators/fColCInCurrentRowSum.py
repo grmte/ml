0dd987f21c0dfac3314a8f0e0435025d61c76c9e
VK <kedia.vikas@gmail.com>
2014-03-25 02:07:34 -0700

    bc

---
+++ ob/generators/fColCInCurrentRowSum.py
@@ -0,0 +1,17 @@
+import dataFile
+import colNumberOfData
+import attribute
+import common
+import aGenArgs
+
+def extractAttributeFromDataMatrix():
+   currentRowCount = 0
+   for dataRow in dataFile.matrix:
+      codeString = 'float(dataFile.matrix[currentRowCount][colNumberOfData.'+aGenArgs.args.c+'0])+float(dataFile.matrix[currentRowCount][colNumberOfData.'+aGenArgs.args.c+'1])+float(dataFile.matrix[currentRowCount][colNumberOfData.'+aGenArgs.args.c+'2])+float(dataFile.matrix[currentRowCount][colNumberOfData.'+aGenArgs.args.c+'3])+float(dataFile.matrix[currentRowCount][colNumberOfData.'+aGenArgs.args.c+'4])'
+      qSum = eval(codeString)
+      attribute.list[currentRowCount][0] = common.getTimeStamp(dataFile.matrix[currentRowCount])
+      attribute.list[currentRowCount][1] = qSum
+      currentRowCount = currentRowCount + 1
+      if (currentRowCount%10000==0):
+         print "Processed row number " + str(currentRowCount)
+
