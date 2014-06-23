7035ad7bfc3a8438c3992803e05aa0b8d4901d7d
dipiaksinghania <dipika@spalgo.com>
2014-06-23 14:00:04 +0530

    DS:Updated

--- src/selectedColumnMarge.py
+++ src/selectedColumnMarge.py
@@ -82,7 +82,7 @@ for target in targetSet:
     targetFiles.append(ltargetFile)
     trrgetFp = trrgetFp + 1
 
-tdFiles = args.td + "*.txt" 
+tdFiles = args.pd + "*.txt" 
 files = glob.glob(tdFiles)  
 trainingFiles = []
 trainingFpList = []
@@ -100,7 +100,7 @@ tradeFpList = []
 for indexOfCL in range(0,len(totalEntryCL)):
     lInitialFileName = fileNamesForTradeDirectory + args.a + '-td.' + os.path.basename(os.path.abspath(args.td)) + \
                    '-dt.' + args.dt + '-targetClass.' + args.targetClass + '-f.' + experimentName + "-wt." + args.wt + \
-                   '-l.'+totalEntryCL[indexOfCL]+"-"+totalExitCL[indexOfCL] + "-te6" + ".trade"
+                   '-l.'+totalEntryCL[indexOfCL]+"-"+totalExitCL[indexOfCL] + "-tq50" + ".trade"
     lTradeFp = open(lInitialFileName, "rb")
     tradeFpList.append(lTradeFp)
     
@@ -108,7 +108,7 @@ dirName = args.pd.replace('/ro/','/rs/')
 fileNamesForTradeDirectory = dirName + "/r/" 
 lInitialFileName = fileNamesForTradeDirectory + args.a + '-td.' + os.path.basename(os.path.abspath(args.td)) + \
                '-dt.' + args.dt + '-targetClass.' + args.targetClass + '-f.' + experimentName + "-wt." + args.wt + \
-               '-l.'+totalEntryCL[indexOfCL]+"-"+totalExitCL[indexOfCL] + "-te6" + ".csv"
+               '-l.'+totalEntryCL[indexOfCL]+"-"+totalExitCL[indexOfCL] + "-tq50" + ".csv"
 print ("filename---", lInitialFileName)
 outputfile = codecs.open(lInitialFileName, 'wb') 
 
