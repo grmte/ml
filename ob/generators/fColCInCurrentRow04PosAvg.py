f28b9f6a755051fad7512e0e5674a57d8c0c4e21
dipiaksinghania <dipika@spalgo.com>
2014-07-10 18:04:17 +0530

    DS:Added feature to find squre root of feature formed

---
+++ ob/generators/fColCInCurrentRow04PosAvg.py
@@ -0,0 +1,21 @@
+
+import dataFile
+import colNumberOfData
+import attribute
+import common
+
+
+def extractAttributeFromDataMatrix(args):
+    currentRowCount = 0
+    list_of_array = [ eval('colNumberOfData.'+args.c+'0') , eval('colNumberOfData.'+args.c+'4')  ]
+    for dataRow in dataFile.matrix:
+        qSum = float(dataRow[list_of_array[0]]) + float(dataRow[list_of_array[4]])
+        qAverage = qSum / 2
+        attribute.aList[currentRowCount][0] = common.getTimeStamp(dataFile.matrix[currentRowCount],colNumberOfData.TimeStamp)
+        attribute.aList[currentRowCount][1] = qAverage
+        currentRowCount = currentRowCount + 1
+        if (currentRowCount%10000==0):
+            print "Processed row number " + str(currentRowCount)
+    
+    lNameOfFeaturePrinted = "fCol" + args.c + "InCurrentRowAvg"
+    return ["TimeStamp",lNameOfFeaturePrinted,"Zero1","Zero2"]
