bc61c3c2a59b93ad0abc8e062fe96ba7e0a3de48
dipiaksinghania <dipika@spalgo.com>
2014-07-10 18:06:14 +0530

    DS:Feature to find 0 and 4th postion average

--- ob/generators/fColCInCurrentRow04PosAvg.py
+++ ob/generators/fColCInCurrentRow04PosAvg.py
@@ -9,7 +9,7 @@ def extractAttributeFromDataMatrix(args):
     currentRowCount = 0
     list_of_array = [ eval('colNumberOfData.'+args.c+'0') , eval('colNumberOfData.'+args.c+'4')  ]
     for dataRow in dataFile.matrix:
-        qSum = float(dataRow[list_of_array[0]]) + float(dataRow[list_of_array[4]])
+        qSum = float(dataRow[list_of_array[0]]) + float(dataRow[list_of_array[1]])
         qAverage = qSum / 2
         attribute.aList[currentRowCount][0] = common.getTimeStamp(dataFile.matrix[currentRowCount],colNumberOfData.TimeStamp)
         attribute.aList[currentRowCount][1] = qAverage
