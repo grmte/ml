aad50e17f29ccd178ce18742b2701ec45745b498
root <root@localhost.localdomain>
2014-07-17 05:49:26 -0400

    AS: updated

--- ob/quality/MtoM.py
+++ ob/quality/MtoM.py
@@ -8,9 +8,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-import time,datetime
-
 import datetime, time
+
+from datetime import timedelta
+from datetime import datetime
 
 def calculate_epoch_time(p_epoch):
     l_dt = datetime.datetime.fromtimestamp(p_epoch)
@@ -101,7 +102,6 @@ def calculate_current_tick_sim_mtm_profit():
                 g_sim_running_profit_short -= (l_trade_price_short * l_trade_qty_short)
                 g_total_sim_traded_price_short += (l_trade_price_short * l_trade_qty_short)
             
-            #fp.write(lineToPrint)
         prviousIndex = index
          
         gross_sim_mtm_profit_long = g_sim_running_profit_long + (float(index[4]) * g_sim_running_qty_long)
@@ -132,8 +132,32 @@ def getDataIntoMatrix(lFileName):
          continue
       dataRow=dataRow.rstrip('\n')
       addDataRowToMatrix(dataRow) 
+
+def getListOfTrainingDirectoriesNames(pNumOfTrainingDays,pStartTrainingDirectory):
+    lTrainingDirectoryList = []
+    l_training_day_folder_base_date = os.path.basename(os.path.abspath(pStartTrainingDirectory))
+    l_start_training_date = datetime.strptime(l_training_day_folder_base_date, '%Y%m%d')
+    index = 0
+    countOfDaysTaken = 0
+    while(1):
+        l_training_date = l_start_training_date + timedelta(days = index)
+        
+        index = index + 1 
+        if( l_training_date.weekday() == 5 or l_training_date.weekday() == 6): # Day is monday
+            continue
+        l_training_date_in_string = l_training_date.strftime('%Y%m%d')
+        l_training_date_full_path_name = pStartTrainingDirectory.replace(l_training_day_folder_base_date,l_training_date_in_string) 
+        print l_training_date_full_path_name
+        if (os.path.exists(l_training_date_full_path_name)):
+            lTrainingDirectoryList.append(l_training_date_full_path_name)
+            countOfDaysTaken += 1
+        if countOfDaysTaken == int(pNumOfTrainingDays):
+            break
+    print lTrainingDirectoryList
+
     
 def main():
+    '''
     global g_sim_gross_mtm_profit_list_long, g_sim_gross_mtm_profit_list_short, g_epoch_timestamp_list
     lFileName = "/home/vikas/ml/ob/data/rs/nsecur/20140703/t/ABFeatureExp/glmnet-td.20140618-dt.10-targetClass.binomial-f.AB-wt.default-l.55-45-tq.300.trade"
     getDataIntoMatrix(lFileName)
@@ -143,7 +167,8 @@ def main():
     
     plot(g_epoch_timestamp_list , g_sim_gross_mtm_profit_list_long , "GROSS_MTM_SIM_FOR_LONG" , "/home/vikas/ml/ob/quality/for-long.png")
     plot(g_epoch_timestamp_list , g_sim_gross_mtm_profit_list_short , "GROSS_MTM_SIM_FOR_SHORT" , "/home/vikas/ml/ob/quality/for-short.png")
-    
+    '''
+    getListOfTrainingDirectoriesNames(10, "/home/vikas/ml/ob/data/rs/nsecur/20140618/")
 
 if __name__ == "__main__":
     main()
