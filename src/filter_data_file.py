08a5db6c957a8ff4552c5661a9f9fa3d49645443
dipikasinghania <dipika@spalgo.com>
2014-08-27 15:13:47 +0530

    DS:Updated

--- src/filter_data_file.py
+++ src/filter_data_file.py
@@ -9,13 +9,19 @@ args = parser.parse_args()
 matrix = []
 def addDataRowToMatrix(pDataRow,pPreviousDataRow):
     dataColumns=pDataRow.split(';')
+    list_temp = []
+    list_temp.extend(dataColumns[0:11])
+    list_temp.extend(dataColumns[13:23])
+    list_temp.extend(dataColumns[25:44])
+    list_temp.extend( [dataColumns[11],dataColumns[12],dataColumns[23],dataColumns[24] ] )
     if pPreviousDataRow==None:
-        matrix.append(dataColumns[0,1,2,3,4,5,6,7,8,9,10,13,14,15,16,17,18,19,20,21,22,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,11,12,23,24])
+        matrix.append( list_temp )
+        #,13,14,15,16,17,18,19,20,21,22,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,11,12,23,24])
     else:
         previousDataColumns = pPreviousDataRow.split(';')
         pPreviousDataRow.split(";")
         if previousDataColumns[1:11] != dataColumns[0:11] or previousDataColumns[13:23] != dataColumns[13:23] :
-            matrix.append(dataColumns[0,1,2,3,4,5,6,7,8,9,10,13,14,15,16,17,18,19,20,21,22,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,11,12,23,24])
+            matrix.append(list)
         
 
 def writeToFile(outputFileName):
