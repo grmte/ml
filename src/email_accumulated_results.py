01425cd5554c378fbe152f88333fb7a6c06905d2
root <root@scp.ao>
2014-09-22 11:40:42 +0530

    DS:Updated

--- src/email_accumulated_results.py
+++ src/email_accumulated_results.py
@@ -46,11 +46,18 @@ def start_mail(p_files,pExperimentNo,message):
     l_msgbody_postfix = "Email sent on : " + str(l_cur_date_time)
     
     l_msg_body = "Dear All, " + "\n\n" + "Kindly find the attachment of ml experiment run . "+ message + " \n\nThanking you.\n\n" + l_msgbody_postfix
-    
+    '''
+    try :
+        import os
+        os.system('gzip -f ' + p_files[0])
+        p_files[0] = p_files[0]+".gz"
+    except:
+        pass
+    '''
     l_subject = "Machine Learning Accumulated results of experiment :- " + pExperimentNo 
     l_send_to = ['ajay@spalgo.com', 'vikas@spalgo.com', 'mike@mbowles.com']
-    l_send_to = ["dipika@spalgo.com"]
-    l_send_cc = ['saptarshi@spalgo.com', 'tulasi@spalgo.com' , 'nikhil@spalgo.com', 'rahul@spalgo.com', 'arabinda@spalgo.com']
+    l_send_to = ["dipika@spalgo.com","tulasi@spalgo.com"]
+    l_send_cc = ['saptarshi@spalgo.com', 'tulasi@spalgo.com' , 'rahul@spalgo.com', 'arabinda@spalgo.com']
     l_send_cc = []
     l_send_from = "MLDailyExperimentResult"
     send_mail(l_send_from, l_send_to, l_send_cc, l_subject, l_msg_body, p_files)
@@ -59,5 +66,5 @@ def start_mail(p_files,pExperimentNo,message):
 if __name__ == '__main__':
     import os
 
-    start_mail(['/home/vikas/ml/ob/e/nsecur/NewABAAvgBAvgHist/Accumulated_Results_for_experiment_NewABAAvgBAvgHist_on_date_29_08_2014.csv.gz'],"ABFeaturesResults","Exp")
+    start_mail(['ob/e/nsecur/NewABAAvgBAvgHist//Accumulated_Results_for_experiment_NewABAAvgBAvgHist_on_date_11_09_2014.csv.gz'],"ABFeaturesResults","Exp")
                 
