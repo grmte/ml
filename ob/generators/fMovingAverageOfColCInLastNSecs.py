2dc7c728ab2d81b814b338ccfff715e3fd684303
dipikasinghania <dipika@spalgo.com>
2014-10-28 13:20:07 +0530

    DS:Updated

--- ob/generators/fMovingAverageOfColCInLastNSecs.py
+++ ob/generators/fMovingAverageOfColCInLastNSecs.py
@@ -2,70 +2,73 @@ import sys, dataFile, colNumberOfData, attribute, common
 from collections import deque
 
 def extractAttributeFromDataMatrix(args):
-   if args.n == None:
-      N = 5
-   else:
-      N = int(args.n) 
-   try:
-      args.c
-   except:
-      print "Since -c has not been specified I cannot proceed"
-      os._exit()
-   if(args.cType == "synthetic"):
-      colNumberOfAttribute = 1
-      colNumberOfTimeStamp = 0
-   else:
-      colNumberOfAttribute = eval("colNumberOfData."+ args.c )
-      colNumberOfTimeStamp = colNumberOfData.TimeStamp
-
-   numberOfRowsInLastNSecs = 0
-   queueOfValuesInLastNSecs = deque()
-   totalOfRowsInLastNSecs = 0.0
-   timeOfOldestRow = common.convertTimeStampFromStringToFloat(dataFile.matrix[0][colNumberOfTimeStamp],args.cType)
-   currentRowNumberForWhichFeatureValueIsBeingCalculated = 0
-   lengthOfDataMatrix = len(dataFile.matrix)
-   while (currentRowNumberForWhichFeatureValueIsBeingCalculated < lengthOfDataMatrix):
-      timeOfCurrentRow = common.convertTimeStampFromStringToFloat(dataFile.matrix[currentRowNumberForWhichFeatureValueIsBeingCalculated][colNumberOfTimeStamp],args.cType)
-      timeElapsed = timeOfCurrentRow - timeOfOldestRow
-      if (timeElapsed < N):
-         cellValue = float(dataFile.matrix[currentRowNumberForWhichFeatureValueIsBeingCalculated][colNumberOfAttribute])
-         totalOfRowsInLastNSecs += cellValue
-         attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][0] = common.convertTimeStampFromStringToDecimal(dataFile.matrix[currentRowNumberForWhichFeatureValueIsBeingCalculated][colNumberOfTimeStamp],args.cType)
-         attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][1] = totalOfRowsInLastNSecs/(numberOfRowsInLastNSecs+1) # in 1st iteration currentRowNumberForWhichFeatureValueIsBeingCalculated = 0
-         attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][2] = str(totalOfRowsInLastNSecs) + ";" + str(numberOfRowsInLastNSecs) + ";" + str(timeElapsed)
-         queueOfValuesInLastNSecs.append([cellValue,timeOfCurrentRow])
-         numberOfRowsInLastNSecs += 1   # Every append gets a +1 
-         currentRowNumberForWhichFeatureValueIsBeingCalculated += 1
-         continue     # Since we are going back 1 row from current we cannot get data from current row
-      else:
-         # We need to reset the timeOfOldestRow since timeElapsed has exceeded N seconds
-         while(timeElapsed >= N):
-            if(len(queueOfValuesInLastNSecs) == 0):
-               timeOfOldestRow = timeOfCurrentRow
-               timeElapsed = 0
-               if(numberOfRowsInLastNSecs != 0):
-                  print "Sanity check: This condition is not possible logically. numberOfRowsInLastNSecs should been 0. There has been an unknown error"
-                  sys.exit(-1)
-               if(totalOfRowsInLastNSecs != 0):
-                  print "Sanity check: This condition is not possible logically. totalOfRowsInLastNSecs should have been 0. There has been an unknown error"
-                  sys.exit(-1)   
-            else:   
-               oldestElementInQueue = queueOfValuesInLastNSecs.popleft()
-               colValueInOldestElementInQueue = oldestElementInQueue[0]
-               totalOfRowsInLastNSecs -= colValueInOldestElementInQueue
-               if len(queueOfValuesInLastNSecs) == 0:
-                    timeElapsed = 0
+    if args.n == None:
+        N = 5
+    else:
+        N = int(args.n) 
+    try:
+        args.c
+    except:
+        print "Since -c has not been specified I cannot proceed"
+        os._exit()
+        
+    if(args.cType == "synthetic"):
+        colNumberOfAttribute = 1
+        colNumberOfTimeStamp = 0
+        colNumberOfExchangeStamp = -1
+    else:
+        colNumberOfAttribute = eval("colNumberOfData."+ args.c )
+        colNumberOfTimeStamp = colNumberOfData.TimeStamp
+        colNumberOfExchangeStamp = colNumberOfData.ExchangeTS
+    
+    numberOfRowsInLastNSecs = 0
+    queueOfValuesInLastNSecs = deque()
+    totalOfRowsInLastNSecs = 0.0
+    timeOfOldestRow = common.convertTimeStampFromStringToFloat(dataFile.matrix[0][colNumberOfExchangeStamp],"synthetic")
+    currentRowNumberForWhichFeatureValueIsBeingCalculated = 0
+    lengthOfDataMatrix = len(dataFile.matrix)
+    while (currentRowNumberForWhichFeatureValueIsBeingCalculated < lengthOfDataMatrix):
+        timeOfCurrentRow = common.convertTimeStampFromStringToFloat(dataFile.matrix[currentRowNumberForWhichFeatureValueIsBeingCalculated][colNumberOfExchangeStamp],"synthetic")
+        timeElapsed = timeOfCurrentRow - timeOfOldestRow
+        if (timeElapsed < N):
+            cellValue = float(dataFile.matrix[currentRowNumberForWhichFeatureValueIsBeingCalculated][colNumberOfAttribute])
+            totalOfRowsInLastNSecs += cellValue
+            attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][0] = common.convertTimeStampFromStringToDecimal(dataFile.matrix[currentRowNumberForWhichFeatureValueIsBeingCalculated][colNumberOfTimeStamp],args.cType)
+            attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][1] = totalOfRowsInLastNSecs/(numberOfRowsInLastNSecs+1) # in 1st iteration currentRowNumberForWhichFeatureValueIsBeingCalculated = 0
+            attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][2] = str(totalOfRowsInLastNSecs) + ";" + str(numberOfRowsInLastNSecs) + ";" + str(timeElapsed) + ";" + str(timeOfCurrentRow)
+            queueOfValuesInLastNSecs.append([cellValue,timeOfCurrentRow])
+            numberOfRowsInLastNSecs += 1   # Every append gets a +1 
+            currentRowNumberForWhichFeatureValueIsBeingCalculated += 1
+            continue     # Since we are going back 1 row from current we cannot get data from current row
+        else:
+            # We need to reset the timeOfOldestRow since timeElapsed has exceeded N seconds
+            while(timeElapsed >= N):
+                if(len(queueOfValuesInLastNSecs) == 0):
                     timeOfOldestRow = timeOfCurrentRow
-               else:
-                    timeOfOldestRow = queueOfValuesInLastNSecs[0][1]
-               numberOfRowsInLastNSecs -= 1 # every pop from the queue gets a -1
-               timeElapsed = timeOfCurrentRow - timeOfOldestRow
-               if(len(queueOfValuesInLastNSecs) != numberOfRowsInLastNSecs):
-                  print "Sanity check: This condition is not possible logically. There has been an unknown error"
-                  sys.exit(-1)
- 
-      print "Processed row number " + str(currentRowNumberForWhichFeatureValueIsBeingCalculated)
-   
-   lNameOfFeaturePrinted = "fMovingAverageOfCol" + args.c + "InLast" + str(args.n) + "Secs"
-   return [ "TimeStamp", lNameOfFeaturePrinted , "TotalOfRowsInLastNSecs" , "NumberOfRowsInLastNSecs" , "TimeElapsed"]
-
+                    timeElapsed = 0
+                    if(numberOfRowsInLastNSecs != 0):
+                        print "Sanity check: This condition is not possible logically. numberOfRowsInLastNSecs should been 0. There has been an unknown error"
+                        sys.exit(-1)
+                    if(totalOfRowsInLastNSecs != 0):
+                        print "Sanity check: This condition is not possible logically. totalOfRowsInLastNSecs should have been 0. There has been an unknown error"
+                        sys.exit(-1)   
+                else:   
+                    oldestElementInQueue = queueOfValuesInLastNSecs.popleft()
+                    colValueInOldestElementInQueue = oldestElementInQueue[0]
+                    totalOfRowsInLastNSecs -= colValueInOldestElementInQueue
+                    if len(queueOfValuesInLastNSecs) == 0:
+                        timeElapsed = 0
+                        timeOfOldestRow = timeOfCurrentRow
+                    else:
+                        timeOfOldestRow = queueOfValuesInLastNSecs[0][1]
+                    numberOfRowsInLastNSecs -= 1 # every pop from the queue gets a -1
+                    timeElapsed = timeOfCurrentRow - timeOfOldestRow
+                    if(len(queueOfValuesInLastNSecs) != numberOfRowsInLastNSecs):
+                        print "Sanity check: This condition is not possible logically. There has been an unknown error"
+                        sys.exit(-1)
+        
+        print "Processed row number " + str(currentRowNumberForWhichFeatureValueIsBeingCalculated)
+    
+    lNameOfFeaturePrinted = "fMovingAverageOfCol" + args.c + "InLast" + str(args.n) + "Secs"
+    return [ "TimeStamp", lNameOfFeaturePrinted , "TotalOfRowsInLastNSecs" , "NumberOfRowsInLastNSecs" , "TimeElapsed"]
+    
--- src/dataFile.py
+++ src/dataFile.py
@@ -2,100 +2,115 @@ import os, sys
 import commands
 matrix = []
 import attribute
+sys.path.append("../ob/generators/")
+import colNumberOfData
 
 def addDataRowToMatrix(pDataRow):
-   dataColumns=pDataRow.split(';')
-   matrix.append(dataColumns)
+    dataColumns=pDataRow.split(';')
+    matrix.append(dataColumns)
 
 def printMatrix():
-   for dataRow in matrix:
-      print "The data row is" , dataRow
+    for dataRow in matrix:
+        print "The data row is" , dataRow
 
 
 def getFileNameFromCommandLineParam(pDirName,pSyntheticColName=""):
-   foundFile=False
-   fileName =""
-   print pSyntheticColName
-   if(pSyntheticColName):
-      pSyntheticColName = pSyntheticColName[1:-1] 
-      pDirName = pDirName.replace("ro","wf") + "/f/" 
-      list_of_files = os.listdir(pDirName) #list of files in the directory                                                            
-      lSyntheticColName = pSyntheticColName
-
-      print "Check Null:-",attribute.instType,"hello"
-      if(attribute.instType!=None):
-         lSyntheticColName = pSyntheticColName + "-iT."+ attribute.instType + "-oT."+attribute.optionsType + "-sP."+attribute.strikePrice
-      for each_file in list_of_files:
-          if lSyntheticColName+".feature" == each_file:
-              print lSyntheticColName+".feature" , each_file
-              fileName = pDirName.replace("ro","wf") +lSyntheticColName+".feature"
-              foundFile = True
-              break
-   else: 
-      try:   
-          if(attribute.instType!=''):
-             command = "ls -1  " +  pDirName + " | grep " +  attribute.instType + "-" + attribute.strikePrice + "-" +  attribute.optionsType
-             print command
-             dataFile = commands.getoutput(command)
-             if dataFile != None:
-                 foundFile = True
-                 fileName = pDirName+"/"+ dataFile
-      except:
-          list_of_files = os.listdir(pDirName) 
-          for each_file in list_of_files:
-                 if each_file.startswith('data') and each_file.endswith('txt'):  #since its all type str you can simply use startswith
+    foundFile=False
+    fileName =""
+    print pSyntheticColName
+    if(pSyntheticColName):
+        pSyntheticColName = pSyntheticColName[1:-1] 
+        pDirName = pDirName.replace("ro","wf") + "/f/" 
+        list_of_files = os.listdir(pDirName) #list of files in the directory                                                            
+        lSyntheticColName = pSyntheticColName
+        
+        print "Check Null:-",attribute.instType,"hello"
+        if(attribute.instType!=None):
+            lSyntheticColName = pSyntheticColName + "-iT."+ attribute.instType + "-oT."+attribute.optionsType + "-sP."+attribute.strikePrice
+        for each_file in list_of_files:
+            if lSyntheticColName+".feature" == each_file:
+                print lSyntheticColName+".feature" , each_file
+                fileName = pDirName.replace("ro","wf") +lSyntheticColName+".feature"
+                foundFile = True
+                break
+    else: 
+        try:   
+            if(attribute.instType!=''):
+                command = "ls -1  " +  pDirName + " | grep " +  attribute.instType + "-" + attribute.strikePrice + "-" +  attribute.optionsType
+                print command
+                dataFile = commands.getoutput(command)
+                if dataFile != None:
+                    foundFile = True
+                    fileName = pDirName+"/"+ dataFile
+        except:
+            list_of_files = os.listdir(pDirName) 
+            for each_file in list_of_files:
+                if each_file.startswith('data') and each_file.endswith('txt'):  #since its all type str you can simply use startswith
                     foundFile = True
                     fileName = pDirName+"/"+each_file
                     break
-   
 
-   if(foundFile != True):
-      print "Did not find the data file" + fileName + "not found"
-      os._exit(-1)
-   else:   
-      print "Data file : "+fileName + " : Found"   
-      sys.stdout.flush()
-      return fileName
+
+    if(foundFile != True):
+        print "Did not find the data file" + fileName + "not found"
+        os._exit(-1)
+    else:   
+        print "Data file : "+fileName + " : Found"   
+        sys.stdout.flush()
+        return fileName
 
 def getDataIntoMatrix(pDirName,pSyntheticColName=""):
-   fileName = getFileNameFromCommandLineParam(pDirName,pSyntheticColName)
-   fileHasHeader = 1
-   headerSkipped = 0
-   for dataRow in open(fileName):
-      if(fileHasHeader == 1 and headerSkipped != 1):
-         headerSkipped = 1 
-         continue
-      dataRow=dataRow.rstrip('\n')
-      addDataRowToMatrix(dataRow)
+    if pSyntheticColName != "":
+        fileName = getFileNameFromCommandLineParam(pDirName,pSyntheticColName)
+        fileHasHeader = 1
+        headerSkipped = 0
+        for dataRow in open(fileName):
+            if(fileHasHeader == 1 and headerSkipped != 1):
+                headerSkipped = 1 
+                continue
+            dataRow=dataRow.rstrip('\n')
+            addDataRowToMatrix(dataRow)
+    else:
+        fileName = getFileNameFromCommandLineParam(pDirName,pSyntheticColName)
+        dataFileName = getFileNameFromCommandLineParam(pDirName)
+        fileHasHeader = 1
+        headerSkipped = 0
+        for dataFileDataRow,exchangeDataRow in zip(open(fileName),open(dataFileName)):
+            if(fileHasHeader == 1 and headerSkipped != 1):
+                headerSkipped = 1 
+                continue
+            dataRow=dataFileDataRow.rstrip('\n') + ";" + exchangeDataRow.strip().split(";")[colNumberOfData.ExchangeTS]
+            addDataRowToMatrix(dataRow)  
+
 
 def getRevelantDataToBeUsedFileName(pDirName):
 
-   pDirName = pDirName.replace('/ro/','/wf/')
-   pDirName = pDirName + "/tr/live_experiment/"
-   command = "ls -1 " + pDirName
-   dataFile = commands.getoutput(command)
-
-   if len(dataFile.split(" ")) > 1 :
-       print dataFile.split(" ")
-       print "More than one target trade file found . Dont know which one tp use"
-       os._exit(-1)
-   return pDirName+dataFile 
+    pDirName = pDirName.replace('/ro/','/wf/')
+    pDirName = pDirName + "/tr/live_experiment/"
+    command = "ls -1 " + pDirName
+    dataFile = commands.getoutput(command)
+    
+    if len(dataFile.split(" ")) > 1 :
+        print dataFile.split(" ")
+        print "More than one target trade file found . Dont know which one tp use"
+        os._exit(-1)
+    return pDirName+dataFile 
 
 def getSelectedDataIntoMatrix(pDirName,pSyntheticColName=""):
-   dataFileName = getFileNameFromCommandLineParam(pDirName,pSyntheticColName)
-   relevantRowDataFileName = getRevelantDataToBeUsedFileName(pDirName)
-   fileHasHeader = 1
-   headerSkipped = 0
-   for dataFileDataRow,relevantRowsData in zip(open(dataFileName),open(relevantRowDataFileName)):
-      if(fileHasHeader == 1 and headerSkipped != 1):
-         headerSkipped = 1 
-         continue
-      if int(relevantRowsData.split(";")[1]) == 1: 
-          dataRow=dataFileDataRow.rstrip('\n')
-          addDataRowToMatrix(dataRow)
+    dataFileName = getFileNameFromCommandLineParam(pDirName,pSyntheticColName)
+    relevantRowDataFileName = getRevelantDataToBeUsedFileName(pDirName)
+    fileHasHeader = 1
+    headerSkipped = 0
+    for dataFileDataRow,relevantRowsData in zip(open(dataFileName),open(relevantRowDataFileName)):
+        if(fileHasHeader == 1 and headerSkipped != 1):
+            headerSkipped = 1 
+            continue
+        if int(relevantRowsData.split(";")[1]) == 1: 
+            dataRow=dataFileDataRow.rstrip('\n')
+            addDataRowToMatrix(dataRow)
       
 def main():
-   getDataIntoMatrix()
+    getDataIntoMatrix()
 
 if __name__ == "__main__":
     main()
