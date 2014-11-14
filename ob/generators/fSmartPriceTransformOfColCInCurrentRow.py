d1bb8d7c98849c98c7ce71ea30294e6c42823621
dipikasinghania <dipika@spalgo.com>
2014-11-14 17:08:40 +0530

    DS:updated:

---
+++ ob/generators/fSmartPriceTransformOfColCInCurrentRow.py
@@ -0,0 +1,39 @@
+import sys, dataFile, colNumberOfData, attribute, common
+
+def extractAttributeFromDataMatrix(args):
+    if args.n == None:
+        N = 5
+    else:
+        N = int(args.n) 
+    try:
+        args.c
+    except:
+        print "Since -c has not been specified I cannot proceed"
+        os._exit()
+        
+    if(args.cType == "synthetic"):
+        colNumberOfAttribute = 1
+        colNumberOfTimeStamp = 0
+        colNumberOfExchangeStamp = -1
+    else:
+        colNumberOfAttribute = eval("colNumberOfData."+ args.c )
+        colNumberOfTimeStamp = colNumberOfData.TimeStamp
+        colNumberOfExchangeStamp = colNumberOfData.ExchangeTS
+    
+    for dataRow in dataFile.matrix:
+        lSmartFeatureValue = float(dataRow[colNumberOfAttribute])
+        lAskP0 = float(dataRow[-2])
+        lBidP0 = float(dataRow[-3])
+        lMidPrice = (lAskP0+lBidP0) / 2
+        if lSmartFeatureValue > lMidPrice:
+            lSmartFeatureValueTransform = lSmartFeatureValue / lAskP0
+        else:
+            lSmartFeatureValueTransform = lBidP0 / lSmartFeatureValue
+        attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][0] = common.convertTimeStampFromStringToDecimal(dataRow[colNumberOfTimeStamp],args.cType)
+        attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][1] = lSmartFeatureValueTransform
+        attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][2] = lAskP0
+        attribute.aList[currentRowNumberForWhichFeatureValueIsBeingCalculated][3] = str(lBidP0) + ";" + str(lSmartFeatureValue)
+
+    lNameOfFeaturePrinted = "fSmartPriceTransformOfCol" + args.c + "InCurrentRow"
+    return [ "TimeStamp", lNameOfFeaturePrinted , "AskP0" , "BidP0" , "lPreviussmartPrice" ]
+         
